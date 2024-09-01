d169e57b795025df88425f4d36d918bcb4b87550
xoorbes <xoorbes@ttsk.ngo>
2024-09-01 17:07:36 +0200

    Changed baseUrl in config to '/swdr5-docs/' for GitHub Pages deployment.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://swdr5.docs.ttsk.ngo',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/swdr5-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
