9e91b9e611976f475ee1ee5b94f6411b6cbe7d81
xoorbes <xoorbes@ttsk.ngo>
2024-09-14 08:18:35 +0200

    feat(docs): add remark-gfm plugin for extended Markdown support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/ttsk-ngo/swdr5-docs/tree/main/",
+          remarkPlugins: [require("remark-gfm")],
         },
         theme: {
           customCss: "./src/css/custom.css",
