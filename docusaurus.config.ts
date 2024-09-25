9dee1b9144da35ef306d682e64fdc0a931978c0a
xoorbes <xoorbes@ttsk.ngo>
2024-09-25 15:42:36 +0200

    feat: integrate Tailwind CSS

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import tailwindPlugin from "./plugins/tailwind-config.cjs";
 
 const config: Config = {
   title: "SWDR 5",
@@ -171,6 +172,7 @@ const config: Config = {
     mermaid: true,
   },
   themes: ["@docusaurus/theme-mermaid"],
+  plugins: [tailwindPlugin],
 };
 
 export default config;
